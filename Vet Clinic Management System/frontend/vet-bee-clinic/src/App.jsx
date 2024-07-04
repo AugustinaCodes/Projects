0203d761243a631d13cfb52d973408ab2cda90b8
August <76185140+Augustina1995@users.noreply.github.com>
2024-07-04 14:40:08 +0300

    set up footer

--- Vet Clinic Management System/frontend/vet-bee-clinic/src/App.jsx
+++ Vet Clinic Management System/frontend/vet-bee-clinic/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Pets from "./components/Pets/Pets";
 import Medications from "./components/Medications/Medications";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/v1/pets" element={<Pets />} />
         <Route path="/v1/medications" element={<Medications />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ Vet Clinic Management System/frontend/vet-bee-clinic/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright © VetBee 2018. All rights reserved.</p>
+    </footer>
+  );
+}
